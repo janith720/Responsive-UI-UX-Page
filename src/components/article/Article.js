7c13fbc2d704ffd506da47db58e1953cf2d0be51
janith720 <janithukwattage@gmail.com>
2022-08-06 07:46:47 +0530

    completed ui ux project

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './article.css'
 
-function Article() {
+function Article({ imgUrl,date,title }) {
   return (
-    <div>Article</div>
+    <div className='gpt3-blog-container-article'>
+        <div className='gpt3-blog-container-article-image'>
+            <img src={imgUrl} alt='blogImage' />
+        </div>
+        <div className='gpt3-blog-container-article-content'>
+            <div>
+              <p>{date}</p>
+              <h3>{title}</h3>
+            </div>
+            <p>Read Full Article</p>
+        </div>
+    </div>
   )
 }
 
